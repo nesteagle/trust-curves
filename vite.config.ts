efa4b277f88828ab47fb1da202390f05c65faeef
nesteagle <104323706+nesteagle@users.noreply.github.com>
2026-06-29 20:18:37 -0700

    Setup React TS + Vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 250,
+    },
+    hmr: {
+      host: "localhost",
+      clientPort: 5173,
+      port: 5173,
+    },
+    host: "0.0.0.0",
+    strictPort: true,
+  },
+});
